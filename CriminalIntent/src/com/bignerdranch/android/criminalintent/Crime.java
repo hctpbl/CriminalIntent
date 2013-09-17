3b3a403cf23754343f6006e661e0c6ec8c210ed4
Héctor Pablos <hctpbl@gmail.com>
2013-09-17 22:58:00 +0200

    Chapter 20 Camera II

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -12,11 +12,13 @@ public class Crime {
 	private static final String JSON_TITLE = "title";
 	private static final String JSON_SOLVED = "solved";
 	private static final String JSON_DATE = "date";
+	private static final String JSON_PHOTO = "photo";
 	
 	private UUID mId;
 	private String mTitle;
 	private Date mDate;
 	private boolean mSolved;
+	private Photo mPhoto;
 	
 	public Crime() {
 		// Generate unique identifier
@@ -29,6 +31,8 @@ public class Crime {
 		mTitle = json.getString(JSON_TITLE);
 		mSolved = json.getBoolean(JSON_SOLVED);
 		mDate = new Date(json.getLong(JSON_DATE));
+		if (json.has(JSON_PHOTO))
+			mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
 	}
 
 	public Date getDate() {
@@ -59,12 +63,22 @@ public class Crime {
 		return mId;
 	}
 	
+	public Photo getPhoto() {
+		return mPhoto;
+	}
+	
+	public void setPhoto(Photo p) {
+		mPhoto = p;
+	}
+	
 	public JSONObject toJSON() throws JSONException {
 		JSONObject json = new JSONObject();
 		json.put(JSON_ID, mId.toString());
 		json.put(JSON_TITLE, mTitle);
 		json.put(JSON_SOLVED, mSolved);
 		json.put(JSON_DATE, mDate.getTime());
+		if (mPhoto != null)
+			json.put(JSON_PHOTO, mPhoto.toJSON());
 		return json;
 	}
 	
